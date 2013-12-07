9af1a251f4ccd5f2ca7c7a9b14d7d22669fdcacc
Henrik Tudborg <henrik@tudb.org>
2013-12-08 00:15:44 +0100

    requirejs, unbind bug fix
    - adding requirejs support (untested)
    - fixer bug where unbinding self inside an event handler would
      cause an event to be skipped (modding an array while looping it!)

--- microevent.js
+++ microevent.js
@@ -2,13 +2,6 @@
 (function () {
     /**
      * MicroEvent - to make any js object an event emitter (server or browser)
-     * 
-     * - pure javascript - server compatible, browser compatible
-     * - dont rely on the browser doms
-     * - super simple - you get it immediatly, no mistery, no magic involved
-     *
-     * - create a MicroEventDebug with goodies to debug
-     *   - make it safer to use
      */
 
     var MicroEvent = function() {};
@@ -24,10 +17,10 @@
             this._events[event].splice(this._events[event].indexOf(fct), 1);
         },
         trigger: function(event /* , args... */ ) {
-            this._events = this._events || {};
-            if (event in this._events === false) return;
-            for (var i = 0; i < this._events[event].length; i++) {
-                this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
+            var events = this._events || {};
+            if (!(event in events)) return;
+            for (var i = 0; i < events[event].length; i++) {
+                events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
             }
         }
     };
@@ -53,6 +46,9 @@
     // export in common js
     if (typeof module !== "undefined" && ('exports' in module)) {
         module.exports = MicroEvent;
+    }
+    else if (typeof define === "function") {
+        define([], MicroEvent);
     }
     else {
         window.MicroEvent = MicroEvent;
